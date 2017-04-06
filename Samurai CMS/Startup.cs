3a80a902c091f05cf73d0dd2f965ab8fbc3c9b31
Filip Cotan <philipcotan@yahoo.com>
2017-04-06 23:31:24 +0300

    Upload project solution to the github repository

---
+++ Samurai CMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Samurai_CMS.Startup))]
+namespace Samurai_CMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
