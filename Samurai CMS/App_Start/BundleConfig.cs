93883ecf3bb2d4751825eee618649c2d5c7a2be2
DESKTOP-Q0HB5G0\Mihai <rmir1955@scs.ubbcluj.ro>
2017-06-05 19:51:51 +0300

    File upload styling

--- Samurai CMS/App_Start/BundleConfig.cs
+++ Samurai CMS/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace Samurai_CMS
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap-filestyle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
